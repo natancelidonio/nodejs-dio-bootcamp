f42d490b9f433ead233d5fd09ffabf95b57d1d1c
Natan Celidonio <natan.c.f.adm@gmail.com>
2024-06-05 18:55:47 -0300

    Modularização de Projetos Node.js com CommonJS - parte 1/2

---
+++ 04-commonjs-modules/src/main.js
@@ -0,0 +1,16 @@
+const product= require("./services/products");
+const config = require("./services/config");
+
+async function main() {
+    console.log("Carrinho de compras:");
+
+    // p.getFullName("408", "mousepad");
+    // p.getFullName("508", "mouse");
+    // p.getProductLabel("mousepad");
+
+    product.getFullName
+    product.productType.version;
+    console.log(config.client);
+};
+
+main();
---
+++ 04-commonjs-modules/src/services/products.js
@@ -0,0 +1,26 @@
+// todas as funções que lidam com produto
+
+const productType ={
+    version: "digital",
+    tax: "x1",
+}
+
+async function getFullName(codeId, productName){
+    console.log("product: " + codeId + "--" + productName);
+    await doBreakLine();
+};
+
+// hidden function
+async function doBreakLine(){
+    console.log("\n");
+};
+
+async function getProductLabel(productName){
+    console.log("Product name: " + productName);
+};
+
+module.exports = {
+    getFullName,
+    getProductLabel,
+    productType,
+};
