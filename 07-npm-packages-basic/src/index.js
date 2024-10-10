cad6826884599ebbf553835a2206e9d507808732
Natan Celidonio <natan.c.f.adm@gmail.com>
2024-10-09 21:53:14 -0300

    NPM Packages Basic. Parte 2/2 - Gerenciando Pacotes com Node

---
+++ 07-npm-packages-basic/src/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+
+console.log(chalk.cyanBright('Hello Blue'));
+console.log(logSymbols.success, chalk.green.italic("Servidor iniciado com sucesso."));
