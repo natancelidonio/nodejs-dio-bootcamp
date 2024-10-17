403153b3be55c0f08144b754a297168d529462f1
Natan Celidonio <natan.c.f.adm@gmail.com>
2024-10-17 10:00:53 -0300

    Gerenciando Variáveis de Ambientes com NPM

---
+++ 09-npm-enviroment/src/database/data.js
@@ -0,0 +1,9 @@
+async function connectToDatabase(user, password) {
+    if (user === process.env.USERDATABASE && password === process.env.PASSWORDDATABASE){
+        console.log("Conexão com banco de dados estabelecida.")
+    } else{
+        console.log("Falha de login, não foi possível se conectar ao banco de dados.")
+    };
+}
+
+export default connectToDatabase;
---
+++ 09-npm-enviroment/src/index.js
@@ -0,0 +1,8 @@
+import connectToDatabase from "./database/data.js";
+
+
+async function main() {
+    await connectToDatabase(process.env.USERDATABASE, process.env.PASSWORDDATABASE);
+}
+
+main();
