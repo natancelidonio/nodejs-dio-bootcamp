f2f7db21cda5be176ded1594f981d972d11636e7
Natan Celidonio <natan.c.f.adm@gmail.com>
2024-10-30 20:51:07 -0300

    TypeScript para Devs Node.js - Como configurar TypeScript em projetos Node.js - parte 1/2

---
+++ 10-typescript/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function main() {
+    let myName = "Natan Celidonio Fernandes";
+    console.log(myName);
+}
+main();
---
+++ 10-typescript/src/index.ts
@@ -0,0 +1,6 @@
+function main() {
+    let myName: string = "Natan Celidonio Fernandes";
+    console.log(myName);
+}
+
+main();
