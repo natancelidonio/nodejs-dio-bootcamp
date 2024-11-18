c5c670057d9a1276cab3200c5979178635bffda1
Natan Celidonio <natan.c.f.adm@gmail.com>
2024-11-18 15:24:34 -0300

    Projeto Gerenciador de Podcasts - refatorando responsabilidade única do server.ts para apenas inicializar o servidor; criado o arquivo app.ts para definir e gerenciar as requisições e services que serão chamadas

---
+++ projeto-gerenciador-podcasts/src/app.ts
@@ -0,0 +1,17 @@
+import * as http from "http";
+import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-controller";
+import { Routes } from "./routes/routes";
+import { httpMethod } from "./utils/http-methods";
+
+export const app = async (req: http.IncomingMessage, res: http.ServerResponse) => {
+        //querystring
+        const [baseUrl, querystring] = req.url?.split("?") ?? ["", ""];
+        //listar todos podcasts
+        if (req.method === httpMethod.GET && baseUrl === Routes.LIST) {
+            await getListEpisodes(req, res);
+        };
+        //filtrar podcasts
+        if (req.method === httpMethod.GET && baseUrl === Routes.EPISODE) {
+            await getFilterEpisodes(req, res);
+        };
+    }
--- projeto-gerenciador-podcasts/src/server.ts
+++ projeto-gerenciador-podcasts/src/server.ts
@@ -1,24 +1,8 @@
 import * as http from "http";
+import { app } from "./app";
 
-import {getFilterEpisodes, getListEpisodes} from "./controllers/podcasts-controller";
-import { Routes } from "./routes/routes";
-import { httpMethod } from "./utils/http-methods";
 
-const server = http.createServer( 
-    async (req: http.IncomingMessage, res: http.ServerResponse) => {
-        
-        //querystring
-        const [baseUrl, querystring] = req.url?.split("?") ?? ["", ""];
-        //listar todos podcasts
-        if(req.method === httpMethod.GET && baseUrl === Routes.LIST) {
-            await getListEpisodes(req, res);
-        };
-        //filtrar podcasts
-        if(req.method === httpMethod.GET && baseUrl === Routes.EPISODE) {
-            await getFilterEpisodes(req, res);
-        };
-    }
-);
+const server = http.createServer(app);
 
 const port = process.env.PORT;
 
