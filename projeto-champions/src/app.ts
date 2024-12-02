d8a827c917ef228abe6c2884744a10c0965fd14b
Natan Celidonio <natan.c.f.adm@gmail.com>
2024-12-02 17:15:55 -0300

    Projeto Recriando API Champions League - implementando CORS

--- projeto-champions/src/app.ts
+++ projeto-champions/src/app.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
+
 
 function createApp() {
     const app = express();
     
     app.use(express.json());
-    app.use("/api", router)
-    
+    app.use("/api", router);
+
+    const corsOptions = {
+        origin: "http://felipao.sistem.com",
+        methods: ["GET"],
+    };
+
+    app.use(cors(corsOptions));
+
     return app;
 }
 
